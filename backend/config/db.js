c4344bee41ea9c4491a8df81008f7740ab249073
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-22 12:00:33 +1000

    420. Conectar la App a la BD

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+// aqui colocamos el string de conexion con la BD y el codigo necesario para conectarnos.
+import mongoose from 'mongoose'
+
+const conectarDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      "mongodb+srv://dbMernTask:dbMernTask@cluster0.redzmok.mongodb.net/uptask",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
+
+    const url = `${connection.connection.host}: ${connection.connection.port}`
+    console.log(`MongoDB Conectado en: ${url}`)
+  } catch (error) {
+    console.log(`error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+export default conectarDB
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 // Aqui va la configuracion  del servidor
-const express = require('express') // va a node_modules, busca el paquete de express y lo asigna a la var.
-// import express from 'express'
+// const express = require('express') // va a node_modules, busca el paquete de express y lo asigna a la var.
+import express from 'express'
+import conectarDB from './config/db.js'
+
 const app = express()
+
+conectarDB()
 
 app.listen(4000, () => {
   console.log('Servidor corriendo en el puerto 4000')
