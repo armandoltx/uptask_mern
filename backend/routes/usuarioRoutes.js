881beb4165f21442131805c6bb21c33d0b296e87
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-22 15:28:27 +1000

    425. Intro a postman; y requests

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
   res.send('Desde API/Usuarios')
 })
 
+router.get('/confirmar', (req, res) => {
+  res.json({ msg: "Confirmando usuario" })
+})
+
+router.post('/', (req, res) => {
+  res.send('Desde post API/Usuarios')
+})
+
 export default router
