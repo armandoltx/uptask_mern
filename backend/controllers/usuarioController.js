d1d7849839075b8b7195200ab40c3f5401beffef
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-22 16:16:13 +1000

    428. Previniendo usuarios duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -2,6 +2,16 @@ import Usuario from "../models/Usuario.js"
 
 const registrar = async (req, res) => {
   // console.log(req.body)
+
+  // Evitar los registros duplicados
+  const { email } = req.body
+  const existeUsuario = await Usuario.findOne({ email }) // ponemos await para q no se vaya a la otra linea hasta q esta no se haya ejecutado
+
+  if(existeUsuario){
+    const error = new Error('Usuario ya registrado')
+    return res.status(400).json({ msg: error.message })
+  }
+
   try {
     const usuario = new Usuario(req.body)
     // console.log(usuario)
