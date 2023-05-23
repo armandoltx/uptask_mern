097aa467d5bc6685fd4403f22b7b8d78129faef7
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-23 16:24:43 +1000

    444. Creando proyectos

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -2,7 +2,20 @@ import Proyecto from "../models/Proyecto.js";
 
 const obtenerProyectos = async (req, res) => {}
 
-const nuevoProyecto = async (req, res) => {}
+const nuevoProyecto = async (req, res) => {
+  // console.log('nuevoProyecto')
+  // console.log(req.body)
+  // console.log(req.usuario)
+  const proyecto = new Proyecto(req.body)
+  proyecto.creador = req.usuario._id
+
+  try {
+    const proyectoAlmacenado = await proyecto.save()
+    res.json(proyectoAlmacenado)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const obtenerProyecto = async (req, res) => {}
 
