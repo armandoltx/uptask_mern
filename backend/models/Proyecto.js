72b98c9735855514e91f75db43bbe36761aec0e9
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-23 15:43:52 +1000

    442. Creando el modelo del proyecto

---
+++ backend/models/Proyecto.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+const proyectoSchema = mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  fechaEntrega: {
+    type: Date,
+    default: Date.now(),
+  },
+  cliente: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  creador: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Usuario"
+  },
+  colaboradores: [ // es un rreglo de usuarios
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario"
+    }
+  ],
+},{
+  timestamps: true
+})
+
+const Proyecto = mongoose.model("Proyecto", proyectoSchema)
+export default Proyecto
