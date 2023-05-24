56e05dcb715d9431eed12fe8277a90149bf25fe2
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-24 11:31:49 +1000

    448. Eliminar un proyecto

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -68,7 +68,29 @@ const editarProyecto = async (req, res) => {
   }
 }
 
-const eliminarProyecto = async (req, res) => {}
+const eliminarProyecto = async (req, res) => {
+  const { id } = req.params
+  // console.log(id)
+  // ver si el proyecto existe
+  const proyecto = await Proyecto.findById(id)
+
+  if(!proyecto) {
+    const error = new Error("Proyecto no Econtrado.")
+    return res.status(404).json({ msg: error.message })
+  }
+
+  if(proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes permiso para ver el Proyecto.")
+    return res.status(401).json({ msg: error.message })
+  }
+
+  try {
+    await proyecto.deleteOne()
+    res.json({ msg: "Proyecto Eliminado." })
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const aregarColaborador = async (req, res) => {}
 
