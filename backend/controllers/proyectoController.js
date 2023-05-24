c9f9fd7e2d78765c04a14c10299e8b8e46c7362a
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-24 15:42:15 +1000

    455. 456. Obtener las tareas

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,4 +1,5 @@
 import Proyecto from "../models/Proyecto.js";
+import Tarea from "../models/Tarea.js";
 
 const obtenerProyectos = async (req, res) => {
   //req.usuario esta creado en el middleware asi q lo podemos usar aqui para el query
@@ -27,23 +28,32 @@ const obtenerProyecto = async (req, res) => {
   // ver si el proyecto existe
   const proyecto = await Proyecto.findById(id)
 
+  // el proyecto exista
   if(!proyecto) {
     const error = new Error("Proyecto no Econtrado.")
     return res.status(404).json({ msg: error.message })
   }
 
+  // es el creador del proyecto
   if(proyecto.creador.toString() !== req.usuario._id.toString()) {
     const error = new Error("No tienes permiso para ver el Proyecto.")
     return res.status(401).json({ msg: error.message })
   }
-  res.json(proyecto)
+
+  // Obtener las tareas
+  const tareas = await Tarea.find().where("proyecto").equals(proyecto._id)
+
+  res.json({
+    proyecto,
+    tareas
+  })
 }
 
 const editarProyecto = async (req, res) => {
   const { id } = req.params
   // console.log(id)
   // ver si el proyecto existe
-  const proyecto = await Proyecto.findById(id)
+  const proyecto = await Proyecto.findById(proyecto._id)
 
   if(!proyecto) {
     const error = new Error("Proyecto no Econtrado.")
@@ -96,7 +106,20 @@ const aregarColaborador = async (req, res) => {}
 
 const eliminarColaborador = async (req, res) => {}
 
-const obtenerTareas = async (req, res) => {}
+const obtenerTareas = async (req, res) => {
+  const { id } = req.params
+  const existeProyecto = await Proyecto.findById(id)
+  if(!existeProyecto) {
+    const error = new Error("No Encontrado")
+    return res.status(404).json({ msg: error.message })
+  }
+
+  // Tienes q ser el creador o el colaborador del proyecto
+  const tareas = await Tarea.find().where("proyecto").equals(id)
+  res.json(tareas)
+
+  // pero es mejor obtener las tareas por proyecto, lo haremos en obtenerProyecto, se puede borrar todo lo relativo a esta accion
+}
 
 export {
   obtenerProyectos,
