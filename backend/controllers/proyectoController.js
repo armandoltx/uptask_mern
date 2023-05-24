ca138c7a62d10aadc82c88768beef9dca2bc97a4
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-24 11:26:41 +1000

    Actualizar proyecto

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -39,7 +39,34 @@ const obtenerProyecto = async (req, res) => {
   res.json(proyecto)
 }
 
-const editarProyecto = async (req, res) => {}
+const editarProyecto = async (req, res) => {
+  const { id } = req.params
+  // console.log(id)
+  // ver si el proyecto existe
+  const proyecto = await Proyecto.findById(id)
+
+  if(!proyecto) {
+    const error = new Error("Proyecto no Econtrado.")
+    return res.status(404).json({ msg: error.message })
+  }
+
+  if(proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes permiso para ver el Proyecto.")
+    return res.status(401).json({ msg: error.message })
+  }
+  // en react vamos a colocar en el state todos los campos, per aqui no:
+  proyecto.nombre = req.body.nombre || proyecto.nombre
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega
+  proyecto.cliente = req.body.cliente || proyecto.cliente
+
+  try {
+    const proyectoAlmacenado = await proyecto.save()
+    res.json(proyectoAlmacenado)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const eliminarProyecto = async (req, res) => {}
 
