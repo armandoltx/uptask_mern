aac704878e504891795ee759e6af0cd92072dc6f
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-25 15:09:40 +1000

    467. Validando q el formulario este lleno y creando el componente de Alerta

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,19 +1,42 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Alerta from '../components/Alerta';
 
 const Registrar = () => {
   const [ nombre, setNombre ] = useState('')
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ repetirPassword, setRepetirPassword ] = useState('')
+  const [ alerta, setAlerta ] = useState({})
+
+  const handleSubmit = e => {
+    // Paramos la accion por default q es submit el formulario
+    e.preventDefault()
+
+    //Hacer q todos los campos sean obligatorios
+    if([nombre, email, password, repetirPassword].includes('')) {
+      // console.log("Todos los campos son obligatorios")
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      })
+      return
+    }
+  }
+
+  const { msg } = alerta
 
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">Crea tu Cuenta y Administra tus {''}
         <span className="text-slate-700">proyectos</span>
       </h1>
+
+      { msg && <Alerta alerta={alerta} /> }
+
       <form
         className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
       >
         <div className="my-5">
           <label
