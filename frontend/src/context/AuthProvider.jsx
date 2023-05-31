0b09e0b7addc2c85c5743fed4fcab4bde85e4f7d
Armando Ruiz <armando.ruiz@industrie.co>
2023-05-31 15:37:17 +1000

    485. Revisando el token para autenticar usuario

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -8,6 +8,35 @@ const AuthContext = createContext()
 const AuthProvider = ({children}) => {
 
   const [ auth, setAuth ] = useState({})
+  useEffect(() => {
+    // para comprobar q haya un token en localstorage
+
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem('token')
+      // console.log(token)
+      // si hay token intentamos autenticar el usuario.
+      if(!token) return
+      // console.log("hay token")
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      try {
+        const { data } = await clienteAxios('/usuarios/perfil', config)
+        // console.log(data)
+        setAuth(data)
+      } catch (error) {
+
+      }
+
+    }
+
+    autenticarUsuario()
+  }, [])
+
   return (
     <AuthContext.Provider
       value={{
