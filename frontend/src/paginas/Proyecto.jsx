312383be3e3707456c80846845d35373a93cc714
Armando Ruiz <armando.ruiz@industrie.co>
2023-06-05 12:01:13 +1000

    508. Mostrando y ocultando el modal desde el context de proyectos

--- frontend/src/paginas/Proyecto.jsx
+++ frontend/src/paginas/Proyecto.jsx
@@ -9,7 +9,7 @@ const Proyecto = () => {
   // hay q pasar el id q viene de params al provider
   // importamos useProyectos, y traemos la funcion obtenerProyecto
   // usamos el  useEffect para comprobar los cambios
-  const { obtenerProyecto, proyecto, cargando } = useProyectos()
+  const { obtenerProyecto, proyecto, cargando, handleModalTarea } = useProyectos()
 
   const [modal, setModal] = useState(false)
 
@@ -38,7 +38,7 @@ const Proyecto = () => {
       </div>
 
       <button
-        onClick={() => setModal(true)}
+        onClick={ handleModalTarea }
         type='button'
         className='text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center'
       >
