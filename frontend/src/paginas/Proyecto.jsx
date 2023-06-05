c7482d5687c63d733cb511924986da548cb0aa52
Armando Ruiz <armando.ruiz@industrie.co>
2023-06-05 11:55:38 +1000

    507. Mostrando el modal

--- frontend/src/paginas/Proyecto.jsx
+++ frontend/src/paginas/Proyecto.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import useProyectos from '../hooks/useProyectos'
 import ModalFormularioTarea from '../components/ModalFormularioTarea'
@@ -10,6 +10,8 @@ const Proyecto = () => {
   // importamos useProyectos, y traemos la funcion obtenerProyecto
   // usamos el  useEffect para comprobar los cambios
   const { obtenerProyecto, proyecto, cargando } = useProyectos()
+
+  const [modal, setModal] = useState(false)
 
   useEffect( () => {
     obtenerProyecto(params.id)
@@ -36,6 +38,7 @@ const Proyecto = () => {
       </div>
 
       <button
+        onClick={() => setModal(true)}
         type='button'
         className='text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center'
       >
@@ -45,7 +48,10 @@ const Proyecto = () => {
         Nueva Tarea
       </button>
 
-      <ModalFormularioTarea />
+      <ModalFormularioTarea
+        modal={modal}
+        setModal={setModal}
+      />
     </>
   )
 };
