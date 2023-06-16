ccc8b01d890a9a944f35e07cf3f2d49e776eeb89
Armando Ruiz <armando.ruiz@industrie.co>
2023-06-16 15:46:51 +1000

    514. Mostrando las Tareas de un Proyecto

---
+++ frontend/src/components/Tarea.jsx
@@ -0,0 +1,30 @@
+import { formatearFecha } from "../helpers/formatearFecha"
+
+const Tarea = ({tarea}) => {
+  const { descripcion, nombre, prioridad, fechaEntrega, estado, _id } = tarea
+
+  return (
+    <div className="border-b p-5 flex justify-between items-center">
+    <div className="flex flex-col  items-start">
+      <p className="mb-1 text-xl">{nombre}</p>
+      <p className="mb-1 text-sm text-gray-500 uppercase">{descripcion}</p>
+      <p className="mb-1 text-sm">{ formatearFecha(fechaEntrega) }</p>
+      <p className="mb-1 text-gray-600">Prioridad: {prioridad}</p>
+    </div>
+    <div className="flex flex-col lg:flex-row gap-2">
+    <button
+      className="bg-indigo-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
+    >Editar</button>
+    <button
+        className={`${estado ? 'bg-sky-600' : 'bg-gray-600'} px-4 py-3 text-white uppercase font-bold text-sm rounded-lg`}
+
+    >{estado ? 'Completa' : 'Incompleta'}</button>
+    <button
+      className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
+    >Eliminar</button>
+    </div>
+    </div>
+  );
+};
+
+export default Tarea;
