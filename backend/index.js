b9cfadba325cbc83e205a808931e9d4c6e79b5cc
Armando Ruiz <armando.ruiz@industrie.co>
2023-07-03 10:48:49 +1000

    544. Agragando socketio a nuestro servidor

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,23 @@ app.use("/api/tareas", tareaRoutes)
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
+
+
+// Socket io
+import { Server } from 'socket.io'
+
+const io = new Server(servidor, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+})
+// Abrimos una conexion de socket io
+io.on("connection", (socket) => {
+  console.log("Conectado a socket.io");
+
+  // Definir los eventos de socket io
 })
